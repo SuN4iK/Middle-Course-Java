d69163b1741a108f66556fe8e0e85a8928fb8c12
SuN4iK <egorka10.08.10@gmail.com>
2025-07-22 19:04:40 +0300

    Первый Коммит

--- src/test/java/NightClub/FaceControlTest.java
+++ src/test/java/NightClub/FaceControlTest.java
@@ -2,7 +2,6 @@ package NightClub;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
