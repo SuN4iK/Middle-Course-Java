ca869e355fe563e0ede770348de7397644bd89a7
SuN4iK <egorka10.08.10@gmail.com>
2025-11-23 19:53:37 +0300

    ХЗ

---
+++ Example/src/main/java/Example/Example.java
@@ -0,0 +1,9 @@
+package Example;
+
+public class Example {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    var person = new Person(13, "Долблат");
+    System.out.println(person.getAge());
+  }
+}
---
+++ Example/src/main/java/Example/Person.java
@@ -0,0 +1,16 @@
+package Example;
+
+import lombok.Getter;
+
+@Getter
+public class Person {
+  private int age;
+  private String name;
+
+  public void Person() {
+    this.age = age;
+    this.name = name;
+  }
+}
+
+
